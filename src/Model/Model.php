129b77050ba7e77cb2c334ebefe6282704a034a9
Szymon Zwoliński <zwolinn@wp.pl>
2020-10-23 00:30:51 +0200

    Add relations

--- src/Model/Model.php
+++ src/Model/Model.php
@@ -5,26 +5,48 @@ namespace ORM\Model;
 abstract class Model {
 
     protected $options;
+    protected $pdo;
+
+    public $relations = [];
 
     public function __construct() {
-        $options = json_encode(file_get_contents('./settings.json', true))['orm'] ;
+        $options = json_encode(file_get_contents('./settings.json', true))['orm'];
+        $this->makePDO();
+    }
+
+    public function getElementById($id) {
+        $stmt = $pdo->prepare('SELECT * FROM '.$this->tableName.$this->getRelationSQL().' WHERE id = :id');
+        $stmt->execute(['id' => $id]);
+        return $stmt->fetch();
+    }
+
+    protected function getRelationSQL(){
+        $sql = '';
+        
+        foreach ($relations as $value) {
+            $sql += ' '.$value->relationType.' '.$value->tableName.' ON '.$this->tableName().'.'.$value->leftSideField.'='.$value->tableName.'.'.$value->rightSideField;
+        }
+
+        return $sql;
     }
 
     protected function makePDO() {
-        $host = '127.0.0.1';
-        $db   = 'test';
-        $user = 'root';
-        $pass = '';
-        $charset = 'utf8mb4';
+        $host = $options['host'];
+        $db   =  $options['db'];
+        $user =  $options['user'];
+        $pass =  $options['pass'];
+        $charset =  $options['charset'];
 
         $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
         $options = [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES   => false,
         ];
+
         try {
-            $pdo = new PDO($dsn, $user, $pass, $options);
+            $this->pdo = new PDO($dsn, $user, $pass, $options);
         } catch (\PDOException $e) {
             throw new \PDOException($e->getMessage(), (int)$e->getCode());
         }
---
+++ src/Model/RelationKey.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ORM\Model;
+
+class RelationKey {
+
+    public $tableName;
+    public $leftSideField;
+    public $rightSideField;
+    public $relationType;
+
+    public function __construct($tableName, $leftSideField, $rightSideField, $relationType="INNER JOIN") {
+        $this->tableName = $tableName;
+        $this->leftSideField = $leftSideField;
+        $this->rightSideField = $rightSideField;
+        $this->$relationType = $relationType;
+    }
+}
